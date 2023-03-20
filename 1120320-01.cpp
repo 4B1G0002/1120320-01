a807a10a7cd5c844a2124e844e27162beb441dd5
士釗 馮 <4b1g0002@gmail.com>
2023-03-20 10:32:48 +0800

    加入專案檔案。

---
+++ 1120320-01.cpp
@@ -0,0 +1,22 @@
+﻿// 1120320-01.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+// ITSA NO-08
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int number;
+
+    cin >> number;
+    string str = to_string(number);
+    string newstring;
+
+    for (int i = str.length() - 1; i >= 0; i--) {
+        newstring.push_back(str[i]);
+    }
+    if (newstring == str) cout << "YES" << endl;
+    else cout << "NO" << endl;
+    return 0;
+}
